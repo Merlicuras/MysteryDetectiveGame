c8b85dd2773252785053fd18b8f24bf9f773fb9b
Camilla <camilla.g.jakobsen@hotmail.com>
2015-04-16 17:06:10 +0200

    MainMenu and GameLobby
    Chosing multiplayer or singleplayer
    Creating or joining multiplayer game function implemented

---
+++ Assets/Scripts/MultiplayerNetwork.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiplayerNetwork : MonoBehaviour {
+
+	public string connectionIP;
+	public int connectionPort;
+	
+	void Start(){
+		connectionIP = "192.168.0.13";
+		connectionPort = 25001;
+	}
+	
+	void Update () {
+	
+	}
+
+	public void CreateGame(){
+		if (Network.peerType == NetworkPeerType.Disconnected){
+			Network.InitializeServer(2, connectionPort, false);
+		}
+	}
+
+	public void JoinGame(){
+		if (Network.peerType == NetworkPeerType.Disconnected){
+			Network.Connect(connectionIP, connectionPort);
+		}
+	}
+
+	public void Disconnect(){
+		if (Network.peerType == NetworkPeerType.Client || Network.peerType == NetworkPeerType.Server){
+			Network.Disconnect(200);
+		}
+	}
+	
+}
