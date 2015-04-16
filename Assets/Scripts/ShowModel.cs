f4fdf879405337140360ea05097748ed52a5a646
Camilla <camilla.g.jakobsen@hotmail.com>
2015-04-16 20:15:27 +0200

    ModelView in CharacterSelect

---
+++ Assets/Scripts/ShowModel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Image))]
+public class ShowModel : MonoBehaviour {
+
+	public Sprite maleSprite;
+	public Sprite femaleSprite;
+
+	private Image model;
+
+	void Start () {
+		model = GameObject.Find("Your Character").GetComponent<Image>();
+		Debug.Log(model.name);
+	}
+	
+	void Update () {
+		
+	}
+
+	public void ShowFemale(){
+		model.overrideSprite = femaleSprite;
+	}
+
+	public void ShowMale(){
+		model.overrideSprite = maleSprite;
+	}
+
+}
