a095a7de513bf532d49b35856c3694e5aaeacce7
Camilla <camilla.g.jakobsen@hotmail.com>
2015-04-16 14:13:37 +0200

    Making network

---
+++ Assets/Scripts/Networking/NetworkMobeObject.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkMobeObject : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (Network.isServer)
+		{
+			Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			float speed = 5;
+			transform.Translate(speed * moveDir * Time.deltaTime);
+		}
+	}
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			Vector3 pos = transform.position;
+			stream.Serialize(ref pos);
+		}
+		else
+		{
+			Vector3 receivedPosition = Vector3.zero;
+			stream.Serialize(ref receivedPosition);
+			transform.position = receivedPosition;
+		}
+	}
+}
---
+++ Assets/Scripts/Networking/Networking.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Networking : MonoBehaviour {
+
+	public string connectionIP = "192.168.0.13";
+	public int connectionPort = 25001;
+
+	void OnGUI (){
+		if(Network.peerType == NetworkPeerType.Disconnected){
+			GUI.Label(new Rect(10, 10, 200, 20), "Status: Disconnected");
+			if(GUI.Button(new Rect(10, 30, 120, 20), "Client Connect")){
+				Network.Connect(connectionIP, connectionPort);
+			}
+			if(GUI.Button(new Rect(10, 50, 120, 20), "Initialize Server")){
+				Network.InitializeServer(2, connectionPort, false);
+			}
+		}
+		else if (Network.peerType == NetworkPeerType.Client){
+			GUI.Label(new Rect(10, 10, 300, 20), "Status: Connected as Client");
+			if(GUI.Button (new Rect(10, 30, 120, 20), "Disconnect")){
+				Network.Disconnect(200);
+			}
+		}
+		else if (Network.peerType == NetworkPeerType.Server){
+			GUI.Label(new Rect(10, 10, 300, 20), "Status: Connected as Server");
+			if(GUI.Button (new Rect(10, 30, 120, 20), "Disconnect")){
+				Network.Disconnect(200);
+			}
+		}
+	}
+
+	void Start () {
+		DontDestroyOnLoad(this);
+	}
+	
+	void Update () {
+	}
+}
