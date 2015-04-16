6c8d77d6dcd1f656fe3d6773cc730eebced6ad60
Camilla <camilla.g.jakobsen@hotmail.com>
2015-04-16 16:24:12 +0200

    Creating scenes to start game

---
+++ Assets/Scripts/ButtonLoadLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonLoadLevel : MonoBehaviour {
+
+	void Start () {
+	}
+	
+	void Update () {
+	}
+
+	public void LoadLevelButton (int index){
+		Application.LoadLevel(index);
+	}
+
+	public void LoadLevelButton (string levelName){
+		Application.LoadLevel(levelName);
+	}
+}
